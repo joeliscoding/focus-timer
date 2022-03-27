94fd114092a1d8d1cfae7e0e47ae5e9699b55c1a
joeliscoding <91632635+joeliscoding@users.noreply.github.com>
2022-03-27 15:00:21 +0200

    fixing bugs for timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let hours, minutes, seconds;
-let insertedHours, insertedMinutes, insertedSeconds;
+let insertedHours, insertedMinutes;
 let isSetup = false;
+const errorMessage = document.getElementById('error-message');
 const timer = {
     el: document.getElementById('timer'),
     interval: 0,
@@ -14,31 +15,34 @@ function insertTime() {
     else hours = 0;
     if(document.getElementById('minutes').value > 0) minutes = document.getElementById('minutes').value;
     else minutes = 0;
-    if(document.getElementById('seconds').value > 0) seconds = document.getElementById('seconds').value;
-    else seconds = 0;
-    if(hours > 0 || minutes > 0 || seconds > 0) {
+    seconds = 0;
+    if((hours > 0 || minutes > 0 || seconds > 0) && minutes < 60 && seconds < 60) {
         setup();
         insertedHours = hours;
         insertedSeconds = seconds;
         insertedMinutes = minutes;
+        isSetup = true;
+        errorMessage.innerHTML = '';
     }
-    else document.getElementById('error').innerHTML = 'Something went wrong. Please check your entries.';
+    else errorMessage.innerHTML = 'Something went wrong. Please check your entries.';
 }
 
 function setup() {
+    if(seconds < 10 && typeof seconds != 'string') seconds = '0' + seconds;
+    if(minutes < 10 && typeof minutes != 'string') minutes = '0' + minutes;
     if(hours > 0) {timer.el.innerHTML = `${hours}:${minutes}:${seconds}`;}
     else {timer.el.innerHTML = `${minutes}:${seconds}`;}
     session.innerHTML += `
     <button id="play-pause-button" type="button" onclick="if(timer.run === false){timer.interval = setInterval(run, 1000); timer.run = true;}"><i class="fa-solid fa-play"></i></button>
     <button type="button" onclick="if(timer.run === true){clearInterval(timer.interval); timer.run = false;}"><i class="fa-solid fa-pause"></i></button>
-    <button type="button" onclick="if(timer.run === true){clearInterval(timer.interval); timer.run = false;} hours = insertedHours; minutes = insertedMinutes; seconds = insertedSeconds; reset()"><i class="fa-solid fa-arrow-rotate-left"></i></button>
+    <button type="button" onclick="if(timer.run === true){clearInterval(timer.interval); timer.run = false;} hours = insertedHours; minutes = insertedMinutes; seconds = 0; reset()"><i class="fa-solid fa-arrow-rotate-left"></i></button>
     
     `;
 }
 
 function run() {
     seconds--;
-    if(seconds < 0 && (minutes > 0 || hours > 0)){
+    if(seconds < 0 && (minutes > 0 || hours > 0)) {
         seconds = 59;
         minutes--;
     }
@@ -46,11 +50,17 @@ function run() {
         minutes = 59;
         hours--;
     }
-    if(hours > 0) {timer.el.innerHTML = `${hours}:${minutes}:${seconds}`;}
-    else {timer.el.innerHTML = `${minutes}:${seconds}`;}
-    if(hours <= 0 && minutes <= 0 && seconds <= 0){
-        clearInterval(timer.interval);
-    }
+    if(seconds < 10 && typeof seconds != 'string') seconds = '0' + seconds;
+    if(minutes < 10 && typeof minutes != 'string') minutes = '0' + minutes;
+
+    if(hours > 0) timer.el.innerHTML = `${hours}:${minutes}:${seconds}`;
+    else timer.el.innerHTML = `${minutes}:${seconds}`;
+
+    if(hours <= 0 && minutes <= 0 && seconds <= 0) clearInterval(timer.interval);
+
+    console.log('sekunden:', typeof seconds);
+    console.log ('minuten:', typeof minutes)
+    console.log('');
 }
 
 function reset() {
