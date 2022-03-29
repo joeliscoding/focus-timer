df23f569b39901b93e7226968208dd01f86e4403
joeliscoding <91632635+joeliscoding@users.noreply.github.com>
2022-03-29 20:33:26 +0200

    start implementing break timer
    started with break timer and added a Ringtone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,24 @@
-let hours, minutes, seconds;
+let hours, minutes, seconds, breakMinutes;
 let insertedHours, insertedMinutes;
 let breaks = {
     bool: false,
     when: 30,
-    duration: 10
+    duration: 10,
+    amount: 0
 }
 let isSetup = false, wasReseted = false;
+let restTime;
+let breakNeeded = false;
+const timeTillBreak = 35, breakDuration = 10; //Angaben in Minuten
 const errorMessage = document.getElementById('error-message');
 const timer = {
     el: document.getElementById('timer'),
     interval: 0,
     run: false
+}
+const breakTimer = {
+    el: document.getElementById('break-timer'),
+    interval: 0
 }
 const session = document.getElementById('session');
 
@@ -22,7 +30,7 @@ function insertTime() {
     else minutes = 0;
     seconds = 0;
     if((hours > 0 || minutes > 0 || seconds > 0) && minutes < 60 && seconds < 60) {
-        setBreaks();
+        setBreak();
         setup();
         insertedHours = hours;
         insertedSeconds = seconds;
@@ -64,7 +72,30 @@ function run() {
     if(hours > 0) timer.el.innerHTML = `${hours}:${minutes}:${seconds}`;
     else timer.el.innerHTML = `${minutes}:${seconds}`;
 
-    if(hours <= 0 && minutes <= 0 && seconds <= 0) clearInterval(timer.interval);
+    if(hours <= 0 && minutes <= 0 && seconds <= 0) {
+        clearInterval(timer.interval);
+        new Audio('/assets/sounds/ringtone.webm').play();
+    }
+}
+
+function runBreakTimer() {
+    if(seconds < 0 && breakMinutes > 0) {
+        seconds = 59;
+        breakMinutes--;
+    }
+    breakTimer.el.innerHTML = `${minutes}:${seconds}`;
+    if(breakMinutes <= 0 && seconds <= 0 && breaks.amount > 0 && breaks.bool === false) {
+        breaks.bool = true;
+        breakMinutes = breaks.duration;
+    }
+    else if(breakMinutes <= 0 && seconds <= 0 && breaks.amount > 0 && breaks.bool === true) {
+        breaks.bool = false;
+        breaks.amount--;
+        breakMinutes = timeTillBreak;
+    }
+    else {
+        clearInterval(breakTimer.interval);
+    }
 }
 
 function reset() {
@@ -74,9 +105,31 @@ function reset() {
     setup();
 }
 
-function setBreaks() {
-    let totalTime = hours * 60 + minutes;
-    if(totalTime > 35) {
-        console.log("pause benötigt")
+function setBreak() {
+    const totalTime = hours * 60 + parseInt(minutes);
+    let timeWithBreak = totalTime;
+    restTime = totalTime;
+    if(totalTime > timeTillBreak) {
+        //console.log("pause benötigt");
+        while(restTime > timeTillBreak) {
+            restTime -= timeTillBreak;
+            breaks.amount++;
+        }
+        //console.log(breaks.amount, 'mal Pause');
+        //console.log(totalTime);
+        //console.log(restTime, 'Minuten zeit nach letzter Pause');
+
+        timeWithBreak += breaks.amount * breakDuration;
+        hours = parseInt(timeWithBreak / 60);
+        minutes = timeWithBreak % 60;
+        breakMinutes = timeTillBreak;
+        breakTimer.el.innerHTML = `${breakMinutes}:${seconds}0`;
+        //breakTimer.interval = setInterval(runBreakTimer, 1000);
+        
+
     }
+    //console.log(timeWithBreak)
+    //console.log(timeWithBreak);
+    //console.log(parseInt(timeWithBreak / 60));
+    //console.log(timeWithBreak % 60);
 }
